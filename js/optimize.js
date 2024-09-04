a3499c5d351547d1db6d317b3a0e38d955e8943e
Joel Dietz <jdietz+github@gmail.com>
2024-09-04 12:03:06 +0200

    add user tracking data on fps drops

--- js/optimize.js
+++ js/optimize.js
@@ -4,6 +4,7 @@ let FRACTALLIGHTNINGDEPTH = 3;
 const desiredFPS = 60;
 let lastFrameTime = performance.now();
 let frameCount = 0;
+let halfFPSCount = 0;
 let fps = 60; // Assume a target of 60 FPS
 const checkInterval = 1000; // Check FPS every 1000ms (1 second)
 
@@ -25,6 +26,7 @@ function adjustEffectIntensity(fps) {
     if (fps < desiredFPS * 0.5) { // If FPS drops below 50% of the target FPS
         // Reduce the intensity of effects significantly
         console.log("fps:" + fps);
+        halfFPSCount++;
         // reduceParticleCount(0.5);
         // lowerEffectResolution(0.5);
     } else if (fps < desiredFPS * 0.75) { // If FPS drops below 75% of the target FPS
