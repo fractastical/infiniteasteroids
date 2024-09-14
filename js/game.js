9023f6757c8f77f5a207d323a1519e4c413d2e15
Joel Dietz <jdietz+github@gmail.com>
2024-09-14 21:23:02 +0200

    version bump

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 
 // for leaderboard and telegram API 
 let gameId = "InfiniteSpaceWar";
-let version = "0.9803"
+let version = "0.9821"
 let crazyGamesMode = false;
 let crazyGamesDebugMode = false;
 
