821c979eb827998f6ca9ced3d3ea3f2ef2e188f5
Joel Dietz <jdietz+github@gmail.com>
2024-09-17 13:17:00 +0200

    micro enhancemetns version

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 
 // for leaderboard and telegram API 
 let gameId = "InfiniteSpaceWar";
-let version = "0.9903"
+let version = "0.9904"
 let crazyGamesMode = false;
 let crazyGamesDebugMode = false;
 let cgUser = null;
