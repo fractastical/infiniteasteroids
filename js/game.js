5e6d8f2cc2a7e890df5fc81eebb26f59a7eb934f
Joel Dietz <jdietz+github@gmail.com>
2024-09-18 11:54:47 +0200

    fix dom load

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 
 // for leaderboard and telegram API 
 let gameId = "InfiniteSpaceWar";
-let version = "0.9922"
+let version = "0.9923"
 let crazyGamesMode = false;
 let crazyGamesDebugMode = false;
 let cgUser = null;
