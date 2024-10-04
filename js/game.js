469de4baf5822ceb553517e4527874e941a9837c
Joel Dietz <jdietz+github@gmail.com>
2024-10-04 16:15:44 +0200

    weaponinfo fits better

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 
 // for leaderboard and telegram API 
 let gameId = "InfiniteSpaceWar";
-let version = "0.9981"
+let version = "0.9982"
 let crazyGamesMode = false;
 let crazyGamesDebugMode = false;
 let normalDebugMode = false;
