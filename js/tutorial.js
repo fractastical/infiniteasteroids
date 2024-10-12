b468f1d0015fdc21c1f323eca540f8321302731e
Joel Dietz <jdietz+github@gmail.com>
2024-10-12 15:25:23 +0200

    move elemental desktop

--- js/tutorial.js
+++ js/tutorial.js
@@ -53,8 +53,8 @@ const desktopTutorialSteps = [
     },
     {
         text: "Destroy the elemental asteroid to see its effect!",
-        position: { top: '35%', left: '75%' },
-        arrowPosition: { top: '30%', left: '75%' },
+        position: { top: '45%', left: '72%' },
+        arrowPosition: { top: '39%', left: '74%' },
         arrowRotation: 0,
         condition: () => elementalAsteroidCreated && elementalAsteroidDestroyed
     },
@@ -127,8 +127,8 @@ const mobileTutorialSteps = [
     },
     {
         text: "Destroy the elemental asteroid to see its effect!",
-        position: { top: '45%', left: '72%' },
-        arrowPosition: { top: '39%', left: '74%' },
+        position: { top: '48%', left: '72%' },
+        arrowPosition: { top: '42%', left: '74%' },
         arrowRotation: 0,
         condition: () => elementalAsteroidCreated && elementalAsteroidDestroyed
     },
